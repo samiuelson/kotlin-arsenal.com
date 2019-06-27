a1549a51bd8975266b95efc9a2082798729d5f69
samuel.urbanowicz <samuel.urbanowicz@netguru.pl>
2019-06-27 12:01:13 +0200

    Implement web frontend PoC

--- webApp/src/CategorySelect.js
+++ webApp/src/CategorySelect.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import FilledInput from '@material-ui/core/FilledInput';
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
@@ -30,9 +25,6 @@ export default function CategorySelect() {
       name: 'hai',
     });
   
-    const inputLabel = React.useRef(null);
-    const [labelWidth, setLabelWidth] = React.useState(0);
-
     function handleChange(event) {
         setValues(oldValues => ({
           ...oldValues,
